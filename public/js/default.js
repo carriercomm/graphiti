ca7c6037995be395f4cdea6bf9f5e4ca12cb3793
Aaron Suggs <aaron@ktheory.com>
2012-06-08 18:20:46 -0400

    Global time is two text fields, allow custom time frames

--- public/js/default.js
+++ public/js/default.js
@@ -13290,29 +13290,29 @@ $("#auto-refresh").change(Graphiti.setRefresh.bind(Graphiti));
 Graphiti = window.Graphiti || {};
 
 Graphiti.initTimeFramePicker = function(){
-  var tf = Graphiti.getTimeFrame();
-  console.log("init to " + tf);
-  if (tf) $("#time-frame option[value='" + tf + "']").attr('selected', 'selected');
+  var el, val;
 
-  $("#time-frame").change(Graphiti.setTimeFrame);
-};
-
-Graphiti.getTimeFrame = function(){
-  return localStorage.getItem("time-frame");
-};
-Graphiti.timeFrameOptions = function(){
-  var tf = Graphiti.getTimeFrame()
-  if (tf) {
-    var ary = tf.split(",");
-    return {"from": ary[0], "until": ary[1] };
-  } else {
-    return {};
+  el = $('#time-frame input[name=from]');
+  if (val = localStorage.getItem('time-frame-from')) {
+    el.val(val);
   }
+  el.change(function(){ localStorage.setItem("time-frame-from", $(this).val()); location.reload(); });
+
+  el = $('#time-frame input[name=until]');
+  if (val = localStorage.getItem('time-frame-until')) {
+    el.val(val);
+  }
+  el.change(function(){ localStorage.setItem("time-frame-until", $(this).val()); location.reload(); });
+
 };
 
-Graphiti.setTimeFrame = function(){
-  localStorage.setItem("time-frame", $(this).val());
-  location.reload();
+Graphiti.timeFrameOptions = function(){
+  var options = {};
+  var from = localStorage.getItem('time-frame-from');
+  if (from) options['from'] = from;
+  var until = localStorage.getItem('time-frame-until');
+  if (until) options['until'] = until;
+  return options;
 };
 
 $(Graphiti.initTimeFramePicker);
